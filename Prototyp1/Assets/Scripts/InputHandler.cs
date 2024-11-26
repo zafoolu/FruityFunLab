c5c6775fa8321eb8a99503c6c572fb739326652b
zafoolu <sologkevin@gmail.com>
2024-11-26 23:49:31 +0100

    Tutorial

--- Prototyp1/Assets/Scripts/InputHandler.cs
+++ Prototyp1/Assets/Scripts/InputHandler.cs
@@ -2,22 +2,56 @@ using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
+    // Referenz zu deinem Tutorial-Screen-GameObject
+    public GameObject tutorialScreen;
+    private bool isTutorialActive;
+
+    void Awake()
+    {
+        // Wenn es ein Tutorial-Screen-GameObject gibt, aktiviere es und setze das Tutorial als aktiv
+        if (tutorialScreen != null)
+        {
+            tutorialScreen.SetActive(true);
+            isTutorialActive = true;
+        }
+        else
+        {
+            // Wenn kein Tutorial-Screen vorhanden ist, ist nichts zu tun
+            isTutorialActive = false;
+            return;
+        }
+    }
+
     void Update()
     {
-        // Überprüft auf beliebige Tasten- oder Mausklicks
-        if (Input.anyKeyDown || Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        // Nur Eingaben prüfen, wenn das Tutorial aktiv ist
+        if (isTutorialActive)
         {
-            Draggable.ResetZoomedCard();
-        }
-
-        // Überprüft Touch-Eingaben
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
+            // Überprüft auf beliebige Tasten- oder Mausklicks
+            if (Input.anyKeyDown || Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
             {
-                Draggable.ResetZoomedCard();
+                DeactivateTutorial();
+            }
+
+            // Überprüft Touch-Eingaben
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Began)
+                {
+                    DeactivateTutorial();
+                }
             }
         }
+    }
+
+    // Funktion zum Deaktivieren des Tutorials
+    private void DeactivateTutorial()
+    {
+        if (tutorialScreen != null)
+        {
+            tutorialScreen.SetActive(false);
+        }
+        isTutorialActive = false;
     }
 }
